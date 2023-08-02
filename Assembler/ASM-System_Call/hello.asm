67df9eec9147f372ec54be3606e39dc9201cbec3
sechshelme <you@example.com>
2023-08-02 19:37:40 +0200

    assembler

--- Assembler/ASM-System_Call/hello.asm
+++ Assembler/ASM-System_Call/hello.asm
@@ -1,9 +1,11 @@
 ; nasm -felf64 hello.asm && ld hello.o && ./a.out
 ; https://cs.lmu.edu/~ray/notes/nasmtutorial/
+; https://codereview.stackexchange.com/questions/206381/simple-puts-function-in-x64-assembly
 
 section  .data
 kmessage: db        27,"[93m",  "Write with kernel interrupt", 10 
-smessage: db        27,"[92m",  "Write with syscall", 10, 10      
+smessage: db        27,"[92m",  "Write with syscall", 10      
+lmessage: db        27,"[91m",  "Write with Lenght", 10, 10, 0      
 
           global    _start
 
@@ -23,8 +25,26 @@ _start:   mov       edx, 33                 ; Message Länge
           mov       rdx, 25                 ; number of bytes
           syscall                           ; invoke operating syst
 
+; ---- with Length
+         mov        rsi, lmessage
+         call       strlen
+         mov        rdi, 1                  ; file handle 1 is stdout
+         mov        rax, 1
+         syscall
+
+       
 ; ----- kernel interrupt Exit
           mov       eax, 1                  ; system call for exit
           int 0x80                          ; Kernel Interrupt
 
 
+
+; --- strlen
+strlen:
+  xor rdx, rdx
+  dec rdx          ; This compensates for the INC that is happening first.
+.next:
+  inc rdx
+  cmp byte [rsi + rdx], 0
+  jne .next
+  ret
